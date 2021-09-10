be19141becc04817a649457fa67352e8770b1e6d
Mateusz Gozdek <mgozdek@microsoft.com>
2021-09-10 14:35:45 +0200

    internal/adapter: test usage of flags message
    As it should be meaningful to the user.
    
    Signed-off-by: Mateusz Gozdek <mgozdek@microsoft.com>

--- internal/adapter/adapter_test.go
+++ internal/adapter/adapter_test.go
@@ -4,6 +4,11 @@
 package adapter_test
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/gsanchezgavier/metrics-adapter/internal/adapter"
@@ -39,6 +44,30 @@ func Test_Creating_adapter(t *testing.T) { //nolint:funlen // Just a lot of test
 		}
 	})
 
+	//nolint:paralleltest // This test overrides stderr, which is global.
+	t.Run("includes_adapter_name_in_usage_message", func(t *testing.T) {
+		helpFlag := "-help"
+
+		options := testOptions()
+		options.Args = []string{helpFlag}
+
+		output, err := captureStderr(t, func() error {
+			_, err := adapter.NewAdapter(options)
+
+			return err //nolint:wrapcheck // We don't care about wrapping in tests.
+		})
+
+		if err == nil {
+			t.Fatalf("Expected error when passing help flag %q", helpFlag)
+		}
+
+		expectedUsage := fmt.Sprintf("Usage of %s", adapter.Name)
+
+		if !strings.Contains(output, expectedUsage) {
+			t.Fatalf("Expected usage message to contain %q, got \n\n%s", expectedUsage, output)
+		}
+	})
+
 	t.Run("returns_error_when", func(t *testing.T) {
 		t.Parallel()
 
@@ -70,4 +99,41 @@ func testOptions() adapter.Options {
 	return adapter.Options{
 		ExternalMetricsProvider: &mock.Provider{},
 	}
+}
+
+// captureStderr is helper function for testing functions, which do not offer specifying where they should write
+// output and write only to os.Stderr. Wrapping such functions using this allows to capture their output as string.
+func captureStderr(t *testing.T, printFunction func() error) (string, error) {
+	t.Helper()
+
+	// Keep backup of the real stdout.
+	old := os.Stderr
+
+	// Get new pipe to temporarily override os.Stderr so we can capture the output.
+	r, w, _ := os.Pipe()
+	os.Stderr = w
+
+	err := printFunction()
+
+	outC := make(chan string)
+
+	// Copy the output in a separate goroutine so printing can't block indefinitely.
+	go func() {
+		var buf bytes.Buffer
+		if _, err := io.Copy(&buf, r); err != nil {
+			t.Logf("Copying strerr: %v", err)
+			t.Fail()
+		}
+		outC <- buf.String()
+	}()
+
+	// Back to normal state.
+	if err := w.Close(); err != nil {
+		t.Fatalf("Closing pipe writer: %v", err)
+	}
+
+	// Restoring the real stdout.
+	os.Stderr = old
+
+	return <-outC, err
 }
