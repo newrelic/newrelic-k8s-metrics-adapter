5418dbe27398f3d8f592682ef29a9f05255e9add
Mateusz Gozdek <mgozdek@microsoft.com>
2021-09-16 18:00:22 +0200

    Add tests for main
    Signed-off-by: Mateusz Gozdek <mgozdek@microsoft.com>

--- main.go
+++ main.go
@@ -4,6 +4,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -18,7 +19,61 @@ import (
 	"github.com/gsanchezgavier/metrics-adapter/internal/provider/newrelic"
 )
 
-const configFileName = "/etc/newrelic/adapter/config.yaml"
+const (
+	// DefaultConfigPath is a path from where configuration will be read.
+	DefaultConfigPath = "/etc/newrelic/adapter/config.yaml"
+
+	// NewRelicAPIKeyEnv is an environment variable name which must be set with a valid NewRelic license key for
+	// adapter to run.
+	NewRelicAPIKeyEnv = "NEWRELIC_API_KEY"
+
+	// ClusterNameEnv is an environment variable name which will be read for filtering cluster-scoped metrics.
+	ClusterNameEnv = "CLUSTER_NAME"
+)
+
+// ConfigOptions represents supported configuration options for metric-adapter.
+type ConfigOptions struct {
+	AccountID       int64                      `json:"accountID"`
+	ExternalMetrics map[string]newrelic.Metric `json:"externalMetrics"`
+}
+
+// Run reads configuration file and environment variables to configure and run the adapter.
+func Run(ctx context.Context, configPath string, args []string) error {
+	config, err := loadConfiguration(configPath)
+	if err != nil {
+		return fmt.Errorf("loading configuration: %w", err)
+	}
+
+	// The NEWRELIC_API_KEY is read from an envVar populated thanks to a k8s secret.
+	c, err := nrClient.New(nrClient.ConfigPersonalAPIKey(os.Getenv(NewRelicAPIKeyEnv)))
+	if err != nil {
+		return fmt.Errorf("creating NewRelic client: %w", err)
+	}
+
+	providerOptions := newrelic.ProviderOptions{
+		ExternalMetrics: config.ExternalMetrics,
+		NRDBClient:      &c.Nrdb,
+		AccountID:       config.AccountID,
+		ClusterName:     os.Getenv(ClusterNameEnv),
+	}
+
+	directProvider, err := newrelic.NewDirectProvider(providerOptions)
+	if err != nil {
+		return fmt.Errorf("creating direct provider: %w", err)
+	}
+
+	options := adapter.Options{
+		Args:                    args,
+		ExternalMetricsProvider: directProvider,
+	}
+
+	a, err := adapter.NewAdapter(options)
+	if err != nil {
+		return fmt.Errorf("initializing adapter: %w", err)
+	}
+
+	return a.Run(ctx.Done()) //nolint:wrapcheck // Don't wrap as otherwise error annotations will be duplicated.
+}
 
 func main() {
 	logs.InitLogs()
@@ -26,59 +81,21 @@ func main() {
 
 	klog.Infof("Starting NewRelic metrics adapter")
 
-	config, err := loadConfiguration()
-	if err != nil {
-		klog.Fatalf("Loading configuration: %v", err)
-	}
-
-	// The NEWRELIC_API_KEY is read from an envVar populated thanks to a k8s secret.
-	c, err := nrClient.New(nrClient.ConfigPersonalAPIKey(os.Getenv("NEWRELIC_API_KEY")))
-	if err != nil {
-		klog.Fatalf("Creating NewRelic client: %v", err)
-	}
-
-	providerOptions := newrelic.ProviderOptions{
-		ExternalMetrics: config.ExternalMetrics,
-		NRDBClient:      &c.Nrdb,
-		AccountID:       config.AccountID,
-		ClusterName:     os.Getenv("CLUSTER_NAME"),
-	}
-
-	directProvider, err := newrelic.NewDirectProvider(providerOptions)
-	if err != nil {
-		klog.Fatalf("Creating direct provider: %v", err)
-	}
-
-	options := adapter.Options{
-		Args:                    os.Args,
-		ExternalMetricsProvider: directProvider,
-	}
-
-	a, err := adapter.NewAdapter(options)
-	if err != nil {
-		klog.Fatalf("Initializing adapter: %v", err)
-	}
-
-	if err := a.Run(signals.SetupSignalHandler().Done()); err != nil {
-		klog.Fatalf("Running adapter: %v", err)
+	if err := Run(signals.SetupSignalHandler(), DefaultConfigPath, os.Args); err != nil {
+		klog.Fatalf("Running adapter failed: %v", err)
 	}
 }
 
-func loadConfiguration() (*configOptions, error) {
-	b, err := ioutil.ReadFile(configFileName)
+func loadConfiguration(configPath string) (*ConfigOptions, error) {
+	b, err := ioutil.ReadFile(configPath)
 	if err != nil {
-		return nil, fmt.Errorf("reading config file %q: %w", configFileName, err)
+		return nil, fmt.Errorf("reading config file: %w", err)
 	}
 
-	config := &configOptions{}
+	config := &ConfigOptions{}
 	if err = yaml.Unmarshal(b, config); err != nil {
 		return nil, fmt.Errorf("unmarshalling config: %w", err)
 	}
 
 	return config, nil
-}
-
-type configOptions struct {
-	AccountID       int64                      `json:"accountID"`
-	ExternalMetrics map[string]newrelic.Metric `json:"externalMetrics"`
 }
