bca450c2135a09a8479b75b27c2b69fe4be12a9b
Juan Jose Jaramillo <juanjjaramillo@yahoo.com>
2023-09-22 15:25:32 -0700

    Address linting issues

--- main.go
+++ main.go
@@ -3,7 +3,6 @@
 
 package main
 
-// TODO: replace io/ioutil - it was deprecated
 import (
 	"context"
 	"errors"
--- main_test.go
+++ main_test.go
@@ -3,7 +3,6 @@
 
 package main_test
 
-// replace io/ioutil - it was deprecated
 import (
 	"context"
 	"fmt"
